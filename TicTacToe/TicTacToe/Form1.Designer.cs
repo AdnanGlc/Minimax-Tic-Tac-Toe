e574aef739f3a2ce5ac9dde2e8221a0abb88c3cb
AdnanGlc <97401156+AdnanGlc@users.noreply.github.com>
2022-06-06 17:27:00 +0200

    Add files via upload

---
+++ TicTacToe/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._00 = new System.Windows.Forms.Button();
+            this._01 = new System.Windows.Forms.Button();
+            this._02 = new System.Windows.Forms.Button();
+            this._12 = new System.Windows.Forms.Button();
+            this._11 = new System.Windows.Forms.Button();
+            this._10 = new System.Windows.Forms.Button();
+            this._22 = new System.Windows.Forms.Button();
+            this._21 = new System.Windows.Forms.Button();
+            this._20 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Restart = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // _00
+            // 
+            this._00.Cursor = System.Windows.Forms.Cursors.Hand;
+            this._00.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._00.Location = new System.Drawing.Point(16, 127);
+            this._00.Margin = new System.Windows.Forms.Padding(4);
+            this._00.Name = "_00";
+            this._00.Size = new System.Drawing.Size(100, 92);
+            this._00.TabIndex = 0;
+            this._00.UseVisualStyleBackColor = false;
+            this._00.Click += new System.EventHandler(this._00_Click);
+            // 
+            // _01
+            // 
+            this._01.Cursor = System.Windows.Forms.Cursors.Hand;
+            this._01.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._01.Location = new System.Drawing.Point(124, 127);
+            this._01.Margin = new System.Windows.Forms.Padding(4);
+            this._01.Name = "_01";
+            this._01.Size = new System.Drawing.Size(100, 92);
+            this._01.TabIndex = 1;
+            this._01.UseVisualStyleBackColor = false;
+            this._01.Click += new System.EventHandler(this._01_Click);
+            // 
+            // _02
+            // 
+            this._02.Cursor = System.Windows.Forms.Cursors.Hand;
+            this._02.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._02.Location = new System.Drawing.Point(232, 127);
+            this._02.Margin = new System.Windows.Forms.Padding(4);
+            this._02.Name = "_02";
+            this._02.Size = new System.Drawing.Size(100, 92);
+            this._02.TabIndex = 2;
+            this._02.UseMnemonic = false;
+            this._02.UseVisualStyleBackColor = false;
+            this._02.Click += new System.EventHandler(this._02_Click);
+            // 
+            // _12
+            // 
+            this._12.Cursor = System.Windows.Forms.Cursors.Hand;
+            this._12.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._12.Location = new System.Drawing.Point(232, 226);
+            this._12.Margin = new System.Windows.Forms.Padding(4);
+            this._12.Name = "_12";
+            this._12.Size = new System.Drawing.Size(100, 92);
+            this._12.TabIndex = 5;
+            this._12.UseVisualStyleBackColor = false;
+            this._12.Click += new System.EventHandler(this._12_Click);
+            // 
+            // _11
+            // 
+            this._11.Cursor = System.Windows.Forms.Cursors.Hand;
+            this._11.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._11.Location = new System.Drawing.Point(124, 226);
+            this._11.Margin = new System.Windows.Forms.Padding(4);
+            this._11.Name = "_11";
+            this._11.Size = new System.Drawing.Size(100, 92);
+            this._11.TabIndex = 4;
+            this._11.UseVisualStyleBackColor = false;
+            this._11.Click += new System.EventHandler(this._11_Click);
+            // 
+            // _10
+            // 
+            this._10.Cursor = System.Windows.Forms.Cursors.Hand;
+            this._10.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._10.Location = new System.Drawing.Point(16, 226);
+            this._10.Margin = new System.Windows.Forms.Padding(4);
+            this._10.Name = "_10";
+            this._10.Size = new System.Drawing.Size(100, 92);
+            this._10.TabIndex = 3;
+            this._10.UseVisualStyleBackColor = false;
+            this._10.Click += new System.EventHandler(this._10_Click);
+            // 
+            // _22
+            // 
+            this._22.Cursor = System.Windows.Forms.Cursors.Hand;
+            this._22.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._22.Location = new System.Drawing.Point(232, 326);
+            this._22.Margin = new System.Windows.Forms.Padding(4);
+            this._22.Name = "_22";
+            this._22.Size = new System.Drawing.Size(100, 92);
+            this._22.TabIndex = 8;
+            this._22.UseVisualStyleBackColor = false;
+            this._22.Click += new System.EventHandler(this._22_Click);
+            // 
+            // _21
+            // 
+            this._21.Cursor = System.Windows.Forms.Cursors.Hand;
+            this._21.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._21.Location = new System.Drawing.Point(124, 326);
+            this._21.Margin = new System.Windows.Forms.Padding(4);
+            this._21.Name = "_21";
+            this._21.Size = new System.Drawing.Size(100, 92);
+            this._21.TabIndex = 7;
+            this._21.UseVisualStyleBackColor = false;
+            this._21.Click += new System.EventHandler(this._21_Click);
+            // 
+            // _20
+            // 
+            this._20.Cursor = System.Windows.Forms.Cursors.Hand;
+            this._20.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._20.Location = new System.Drawing.Point(16, 326);
+            this._20.Margin = new System.Windows.Forms.Padding(4);
+            this._20.Name = "_20";
+            this._20.Size = new System.Drawing.Size(100, 92);
+            this._20.TabIndex = 6;
+            this._20.UseVisualStyleBackColor = false;
+            this._20.Click += new System.EventHandler(this._20_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(16, 426);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(315, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // Restart
+            // 
+            this.Restart.Location = new System.Drawing.Point(16, 462);
+            this.Restart.Margin = new System.Windows.Forms.Padding(4);
+            this.Restart.Name = "Restart";
+            this.Restart.Size = new System.Drawing.Size(100, 28);
+            this.Restart.TabIndex = 10;
+            this.Restart.Text = "Restart";
+            this.Restart.UseVisualStyleBackColor = true;
+            this.Restart.Click += new System.EventHandler(this.Restart_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Dva igraca",
+            "Jedan igrac"});
+            this.comboBox1.Location = new System.Drawing.Point(132, 464);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(203, 24);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Unesite ime prvog igraca";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(16, 47);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(315, 22);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.Text = "X";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(16, 95);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(315, 22);
+            this.textBox3.TabIndex = 15;
+            this.textBox3.Text = "O";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 75);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 17);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Unesite ime drugog igraca";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(349, 500);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Restart);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this._22);
+            this.Controls.Add(this._21);
+            this.Controls.Add(this._20);
+            this.Controls.Add(this._12);
+            this.Controls.Add(this._11);
+            this.Controls.Add(this._10);
+            this.Controls.Add(this._02);
+            this.Controls.Add(this._01);
+            this.Controls.Add(this._00);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "TicTacToe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _00;
+        private System.Windows.Forms.Button _01;
+        private System.Windows.Forms.Button _02;
+        private System.Windows.Forms.Button _12;
+        private System.Windows.Forms.Button _11;
+        private System.Windows.Forms.Button _10;
+        private System.Windows.Forms.Button _22;
+        private System.Windows.Forms.Button _21;
+        private System.Windows.Forms.Button _20;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Restart;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TicTacToe/TicTacToe/Form1.cs
@@ -0,0 +1,376 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TicTacToe.Properties;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        int Igrac = 0; 
+        bool Kompjuter=false;
+        char[] xo = new char[2] { 'X', 'O' };
+        string[] igraci = new string[2] { "X", "O" };
+        char[,] Ploca=new char[3,3]{ {' ',' ',' '},{' ',' ',' '},{' ',' ',' '} };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool Pobjeda()
+        {//ako su ista 3 znaka horiznotalno ili veritkalo ili po diagonalama u istom redu onda je pobijedio igrac koji je zadnji upisao znak
+            if ((Ploca[0, 0] == Ploca[0, 1] && Ploca[0, 0] == Ploca[0, 2] && Ploca[0, 0] != ' ')
+             || (Ploca[1, 0] == Ploca[1, 1] && Ploca[1, 0] == Ploca[1, 2] && Ploca[1, 0] != ' ')
+             || (Ploca[2, 0] == Ploca[2, 1] && Ploca[2, 0] == Ploca[2, 2] && Ploca[2, 0] != ' ')//horizontalno
+             || (Ploca[0, 0] == Ploca[1, 0] && Ploca[1, 0] == Ploca[2, 0] && Ploca[0, 0] != ' ')
+             || (Ploca[0, 1] == Ploca[1, 1] && Ploca[0, 1] == Ploca[2, 1] && Ploca[0, 1] != ' ')
+             || (Ploca[0, 2] == Ploca[1, 2] && Ploca[0, 2] == Ploca[2, 2] && Ploca[0, 2] != ' ')//vertikalno
+             || (Ploca[0, 0] == Ploca[1, 1] && Ploca[0, 0] == Ploca[2, 2] && Ploca[0, 0] != ' ')
+             || (Ploca[0, 2] == Ploca[1, 1] && Ploca[0, 2] == Ploca[2, 0] && Ploca[0, 2] != ' ')//dijagonale
+                ) return true;
+            else return false;
+        }
+        bool Ima_Poteza(StringBuilder s)
+        {
+            for (int i = 0; i < 9; i++)
+                if (s[i] == ' ') return true;
+            return false;
+        }
+        int Procijeni(StringBuilder s)
+        {
+            for(int i=0;i<3;i++)
+            {
+                if(s[i*3]==s[i*3+1] && s[i*3]==s[i*3+2])//horizontalno
+                {
+                    if (s[i * 3] == xo[Igrac % 2]) return +10;
+                    else if (s[i * 3] == xo[(Igrac + 1) % 2]) return -10;
+                }
+                if(s[i]==s[i+3] && s[i]==s[i+6])//verikalno
+                {
+                    if (s[i] == xo[Igrac % 2]) return +10;
+                    else if (s[i] == xo[(Igrac + 1) % 2]) return -10;
+                }
+            }
+            //dijagonale
+            if(s[0]==s[4] && s[0]==s[8])
+            {
+                if (s[0] == xo[Igrac % 2]) return +10;
+                else if (s[0] == xo[(Igrac + 1) % 2]) return -10;
+            }
+            if (s[2] == s[4] && s[2] == s[6])
+            {
+                if (s[2] == xo[Igrac % 2]) return +10;
+                else if (s[2] == xo[(Igrac + 1) % 2]) return -10;
+            }
+            return 0;
+
+        }
+        int minimax(StringBuilder s,bool IsMax)
+        {
+            int score = Procijeni(s);
+            if (score == 10 || score == -10) return score;
+            if (Ima_Poteza(s) == false) return 0;
+            
+            if(IsMax)
+            {
+                int best = -1000;
+                for(int i=0;i<9;i++)
+                {
+                    if(s[i]==' ')
+                    {
+                        s[i] = xo[Igrac % 2];
+                        best = Math.Max(best, minimax(s, !IsMax));
+                        s[i] = ' ';
+                    }
+                }
+                return best;
+            }
+            else
+            {
+                int best = 1000;
+                for (int i = 0; i < 9; i++)
+                {
+                    if (s[i] == ' ')
+                    {
+                        s[i] = xo[(Igrac+1) % 2];
+                        best = Math.Min(best, minimax(s, !IsMax));
+                        s[i] = ' ';
+                    }
+                }
+                return best;
+            }
+        }
+        void OdigrajNajboljiPotez()
+        {
+            StringBuilder s = new StringBuilder();
+            s.Clear();
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    s.Append(Ploca[i, j]);
+            int Najbojli = -1000;
+            int NajboljiX = -1, NajboljiY = -1;
+            for(int i=0;i<9;i++)
+            {
+                if(s[i]==' ')
+                {
+                    s[i] = xo[Igrac % 2];
+                    int test = minimax(s, false);
+                    s[i] = ' ';
+                    if(test>Najbojli)
+                    {
+                        Najbojli = test;
+                        NajboljiX = i / 3;
+                        NajboljiY = i % 3;
+                    }
+                }
+            }
+            OdigrajPotez(NajboljiX, NajboljiY);
+        }
+        void OdigrajPotez(int i,int j)
+        {
+            if (i == -1) return;
+            if(i==0 && j==0)
+            {
+                Ploca[0, 0] = xo[Igrac % 2]; 
+                _00.Text = xo[Igrac % 2].ToString();
+                _00.Enabled = false;
+            }
+            if (i == 0 && j == 1)
+            {
+                Ploca[0, 1] = xo[Igrac % 2];
+                _01.Text = xo[Igrac % 2].ToString();
+                _01.Enabled = false;
+            }
+            if (i == 0 && j == 2)
+            {
+                Ploca[0, 2] = xo[Igrac % 2];
+                _02.Text = xo[Igrac % 2].ToString();
+                _02.Enabled = false;
+            }
+            ///
+            if (i == 1 && j == 0)
+            {
+                Ploca[1, 0] = xo[Igrac % 2];
+                _10.Text = xo[Igrac % 2].ToString();
+                _10.Enabled = false;
+            }
+            if (i == 1 && j == 1)
+            {
+                Ploca[1, 1] = xo[Igrac % 2];
+                _11.Text = xo[Igrac % 2].ToString();
+                _11.Enabled = false;
+            }
+            if (i == 1 && j == 2)
+            {
+                Ploca[1, 2] = xo[Igrac % 2];
+                _12.Text = xo[Igrac % 2].ToString();
+                _12.Enabled = false;
+            }
+            ///
+            if (i == 2 && j == 0)
+            {
+                Ploca[2, 0] = xo[Igrac % 2];
+                _20.Text = xo[Igrac % 2].ToString();
+                _20.Enabled = false;
+            }
+            if (i == 2 && j == 1)
+            {
+                Ploca[2, 1] = xo[Igrac % 2];
+                _21.Text = xo[Igrac % 2].ToString();
+                _21.Enabled = false;
+            }
+            if (i == 2 && j == 2)
+            {
+                Ploca[2, 2] = xo[Igrac % 2];
+                _22.Text = xo[Igrac % 2].ToString();
+                _22.Enabled = false;
+            }
+            if (Pobjeda())
+            {
+                textBox1.Text = "Pobjednik je igrac " + igraci[Igrac % 2];
+                DisableSvaDugmad();
+                return;
+            }
+            else if (Igrac == 8) textBox1.Text = "Nerjeseno";
+
+            Igrac++;
+        }
+        void DisableSvaDugmad()
+        { //na kraju igre iskljucimo sva dugmad
+            _00.Enabled = false;
+            _01.Enabled = false;
+            _02.Enabled = false;
+            _10.Enabled = false;
+            _11.Enabled = false;
+            _12.Enabled = false;
+            _20.Enabled = false;
+            _21.Enabled = false;
+            _22.Enabled = false;
+        }
+        private void _00_Click(object sender, EventArgs e)
+        { 
+            OdigrajPotez(0, 0);
+            if(Kompjuter==true)
+            {
+                OdigrajNajboljiPotez();
+            }
+        }
+
+        private void _01_Click(object sender, EventArgs e)
+        {
+            OdigrajPotez(0, 1);
+            if (Kompjuter == true)
+            {
+                OdigrajNajboljiPotez();
+            }
+
+        }
+
+        private void _02_Click(object sender, EventArgs e)
+        {
+            OdigrajPotez(0, 2);
+            if (Kompjuter == true)
+            {
+                OdigrajNajboljiPotez();
+            }
+        }
+
+        private void _10_Click(object sender, EventArgs e)
+        {
+            OdigrajPotez(1, 0);
+            if (Kompjuter == true)
+            {
+                OdigrajNajboljiPotez();
+            }
+        }
+
+        private void _11_Click(object sender, EventArgs e)
+        {
+            OdigrajPotez(1, 1);
+            if (Kompjuter == true)
+            {
+                OdigrajNajboljiPotez();
+            }
+        }
+
+        private void _12_Click(object sender, EventArgs e)
+        {
+            OdigrajPotez(1, 2);
+            if (Kompjuter == true)
+            {
+                OdigrajNajboljiPotez();
+            }
+        }
+
+        private void _20_Click(object sender, EventArgs e)
+        {
+            OdigrajPotez(2, 0);
+            if (Kompjuter == true)
+            {
+                OdigrajNajboljiPotez();
+            }
+        }
+
+        private void _21_Click(object sender, EventArgs e)
+        {
+            OdigrajPotez(2, 1);
+            if (Kompjuter == true)
+            {
+                OdigrajNajboljiPotez();
+            }
+        }
+
+        private void _22_Click(object sender, EventArgs e)
+        {
+            OdigrajPotez(2, 2);
+            if (Kompjuter == true)
+            {
+                OdigrajNajboljiPotez();
+            }
+        }
+
+        private void Restart_Click(object sender, EventArgs e)
+        {//kada restartujemo igru omogucimo sva dugmad
+            _00.Enabled = true;
+            _01.Enabled = true;
+            _02.Enabled = true;
+            _10.Enabled = true;
+            _11.Enabled = true;
+            _12.Enabled = true;
+            _20.Enabled = true;
+            _21.Enabled = true;
+            _22.Enabled = true;
+            //
+            _00.Text = "";//obrisemo text iz svih dugmadi
+            _01.Text = "";
+            _02.Text = "";
+            _10.Text = "";
+            _11.Text = "";
+            _12.Text = "";
+            _20.Text = "";
+            _21.Text = "";
+            _22.Text = "";
+            //
+            textBox1.Text = ""; // i text boxa
+            //
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Ploca[i, j] = ' ';
+                }
+            }
+            //
+            Igrac = 0;
+            Kompjuter = false;
+            if (comboBox1.SelectedIndex == 1)
+            {
+                Kompjuter = true;
+                Random rnd = new Random();
+                if (rnd.Next(0, 2) == 1)
+                {
+                    OdigrajNajboljiPotez();
+                }
+            }
+            //
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                igraci[0] = Convert.ToString(textBox2.Text);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                igraci[1] = Convert.ToString(textBox3.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Text = "Dva igraca";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.Restart_Click(this, null);
+        }
+    }
+}
